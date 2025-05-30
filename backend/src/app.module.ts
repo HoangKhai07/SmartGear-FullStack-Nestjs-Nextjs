5bd037bc69d61c44bd5f6bc61f2e49e468236e8b
KhaiNoob <danhhoangkhai07@gmail.com>
2025-05-30 10:51:52 +0700

    feat: create user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
+import { pinoHttp } from 'pino-http';
+
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (ConfigService: ConfigService) => {
+        const isProduction = ConfigService.get('NODE_ENV') === 'production'
+        return {
+          pinoHttp: {
+            transport: isProduction ? undefined : {
+              target: 'pino-pretty',
+              options: {
+                singleLine: true,
+              }
+            },
+            level: isProduction ? 'info' : 'debug'
+          }
+        }
+      },
+      inject: [ConfigService]
+    }),
+    ConfigModule.forRoot(), UsersModule],
   controllers:[],
+  providers: [],
 })
 export class AppModule {}
 
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserRequest } from 'src/users/dto/create-user.requets';
+import { Prisma, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
-    createUser(data: CreateUserRequest ) {
-        console.log(data)
+    constructor (private readonly prismaService: PrismaService){}
+    async createUser(data: CreateUserRequest ) : Promise<User> {
+       try {
+        return await this.prismaService.user.create({
+            data: {
+                ...data,    
+                password: await bcrypt.hash(data.password, 10)
+            },
+        })
+       } catch (err) {
+        if(err.code === "P2002") {
+            throw new UnauthorizedException("Email already exists")
+        }
+        throw err;
+       }
     }
 }
